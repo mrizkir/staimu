a5fcb4b00e812c30772d9c7bd669ce1c5e2704af
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2020-08-18 08:32:53 +0700

    dashboard keuangan 2

--- frontend/src/views/pages/admin/keuangan/Keuangan.vue
+++ frontend/src/views/pages/admin/keuangan/Keuangan.vue
@@ -32,6 +32,80 @@
             <Filter1 v-on:changeTahunAkademik="changeTahunAkademik" ref="filter1" />
         </template>
         <v-container fluid>
+            <v-row dense>
+                <v-col xs="12" sm="4" md="3">
+                    <v-card                         
+                        class="clickable"
+                        color="#385F73" 
+                        @click.native="$router.push('/spmb/pendaftaranbaru')"
+                        dark>
+                        <v-card-title class="headline">
+                            TOTAL TRANSAKSI
+                        </v-card-title>    
+                        <v-card-subtitle>
+                            Total transaksi keseluruhan
+                        </v-card-subtitle>
+                        <v-card-text>
+                            {{total_transaction|formatUang}}
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly"/>  
+                <v-col xs="12" sm="4" md="3">
+                    <v-card                         
+                        class="clickable"
+                        color="#385F73" 
+                        @click.native="$router.push('/spmb/pendaftaranbaru')"
+                        dark>
+                        <v-card-title class="headline">
+                            TRANSAKSI PAID
+                        </v-card-title>    
+                        <v-card-subtitle>
+                            Total transaksi dengan status PAID
+                        </v-card-subtitle>
+                        <v-card-text>
+                            {{total_transaction_paid|formatUang}}
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly"/>  
+                <v-col xs="12" sm="4" md="3">
+                    <v-card                         
+                        class="clickable"
+                        color="#385F73" 
+                        @click.native="$router.push('/spmb/pendaftaranbaru')"
+                        dark>
+                        <v-card-title class="headline">
+                            TRANSAKSI UNPAID
+                        </v-card-title>    
+                        <v-card-subtitle>
+                            Total transaksi dengan status UNPAID
+                        </v-card-subtitle>
+                        <v-card-text>
+                            {{total_transaction_unpaid|formatUang}}
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly"/>  
+                <v-col xs="12" sm="4" md="3">
+                    <v-card                         
+                        class="clickable"
+                        color="#385F73" 
+                        @click.native="$router.push('/spmb/pendaftaranbaru')"
+                        dark>
+                        <v-card-title class="headline">
+                            TRANSAKSI CANCELLED
+                        </v-card-title>    
+                        <v-card-subtitle>
+                            Total transaksi dengan status CANCELLED
+                        </v-card-subtitle>
+                        <v-card-text>
+                            {{total_transaction_cancelled|formatUang}}
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly"/>  
+            </v-row>
             <v-row>
                 <v-col xs="12" sm="12" md="6">
                     <v-card class="mb-3">
@@ -49,14 +123,14 @@
                                 :disable-pagination="true"
                                 :hide-default-footer="true"
                                 :headers="headers"
-                                :items="kombi_ganjil"> 
+                                :items="kombi_ganjil_paid"> 
                                 <template v-slot:item.jumlah="{ item }">                            
                                     {{  item.jumlah|formatUang }}
                                 </template>    
-                                <template v-slot:body.append v-if="kombi_ganjil.length > 0">
+                                <template v-slot:body.append v-if="kombi_ganjil_paid.length > 0">
                                     <tr class="grey lighten-4 font-weight-black">
                                         <td class="text-right">TOTAL</td>
-                                        <td class="text-right">{{totalKombiGanjil|formatUang}}</td>                                                                                
+                                        <td class="text-right">{{totalKombiGanjilPaid|formatUang}}</td>                                                                                
                                     </tr>
                                 </template>
                                 <template v-slot:no-data>                            
@@ -64,7 +138,10 @@
                                 </template>                           
                             </v-data-table>
                         </v-card-text>
-                    </v-card>
+                    </v-card>                    
+                </v-col>
+                <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly"/>  
+                <v-col xs="12" sm="12" md="6">
                     <v-card class="mb-3">
                         <v-card-title>
                             Semester Genap
@@ -80,14 +157,14 @@
                                 :disable-pagination="true"
                                 :hide-default-footer="true"
                                 :headers="headers"
-                                :items="kombi_genap"> 
+                                :items="kombi_genap_paid"> 
                                 <template v-slot:item.jumlah="{ item }">                            
                                     {{  item.jumlah|formatUang }}
                                 </template>    
-                                <template v-slot:body.append v-if="kombi_genap.length > 0">
+                                <template v-slot:body.append v-if="kombi_genap_paid.length > 0">
                                     <tr class="grey lighten-4 font-weight-black">
                                         <td class="text-right">TOTAL</td>
-                                        <td class="text-right">{{totalKombiGenap|formatUang}}</td>                                                                                
+                                        <td class="text-right">{{totalKombiGenapPaid|formatUang}}</td>                                                                                
                                     </tr>
                                 </template>
                                 <template v-slot:no-data>                            
@@ -96,10 +173,6 @@
                             </v-data-table>
                         </v-card-text>
                     </v-card>
-                </v-col>
-                <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly"/>  
-                <v-col xs="12" sm="12" md="6">
-                    
                 </v-col>
                 <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly"/>  
             </v-row>
@@ -139,12 +212,24 @@ export default {
         tahun_akademik:0,
         
         //daftar komponen biaya
-        kombi_ganjil:[],       
-        kombi_genap:[],       
+        kombi_ganjil_unpaid:[],       
+        kombi_genap_unpaid:[],       
+
+        kombi_ganjil_paid:[],       
+        kombi_genap_paid:[],       
+
+        kombi_ganjil_cancelled:[],       
+        kombi_genap_cancelled:[],       
+
         headers: [                        
             { text: 'NAMA KOMPONEN', value: 'nama_kombi', sortable:false},               
             { text: 'JUMLAH', align:'end',value: 'jumlah',width:250, sortable:false},                
         ], 
+        //statistik
+        total_transaction:0,
+        total_transaction_paid:0,
+        total_transaction_unpaid:0,
+        total_transaction_cancelled:0
     }),
     methods : {
         changeTahunAkademik (tahun)
@@ -162,33 +247,47 @@ export default {
                 headers: {
                     Authorization:this.$store.getters['auth/Token']
                 }
-            }).then(({data})=>{                            
-                this.kombi_ganjil=data.kombi_ganjil;
+            }).then(({data})=>{                 
+                this.total_transaction=data.total_transaction;
+                this.total_transaction_paid=data.total_transaction_paid;          
+                this.total_transaction_unpaid=data.total_transaction_unpaid;          
+                this.total_transaction_cancelled=data.total_transaction_cancelled;          
+
+                this.kombi_ganjil_unpaid=data.kombi_ganjil_unpaid;
+                this.kombi_genap_unpaid=data.kombi_genap_unpaid;
+
+                this.kombi_ganjil_paid=data.kombi_ganjil_paid;
+                this.kombi_genap_paid=data.kombi_genap_paid;
+
+                this.kombi_ganjil_cancelled=data.kombi_ganjil_cancelled;
+                this.kombi_genap_cancelled=data.kombi_genap_cancelled;
+                
                 this.datatableLoading=false;
             }).catch(()=>{
                 this.datatableLoading=false;
             });
             this.firstloading=false;            
             this.$refs.filter1.setFirstTimeLoading(this.firstloading); 
+
         }
     },
-    computed:{
-        totalKombiGanjil()
+    computed:{        
+        totalKombiGanjilPaid()
         {
             var total = 0;
-            for (var i =0; i < this.kombi_ganjil.length; i++)
+            for (var i =0; i < this.kombi_ganjil_paid.length; i++)
             {
-                var item = this.kombi_ganjil[i];
+                var item = this.kombi_ganjil_paid[i];
                 total+=item.jumlah;
             }
             return total;
         },
-        totalKombiGenap()
+        totalKombiGenapPaid()
         {
             var total = 0;
-            for (var i =0; i < this.kombi_genap.length; i++)
+            for (var i =0; i < this.kombi_genap_paid.length; i++)
             {
-                var item = this.kombi_genap[i];
+                var item = this.kombi_genap_paid[i];
                 total+=item.jumlah;
             }
             return total;
