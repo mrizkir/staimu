69aab9f2424f30b0345c8e08fc137960674b14ef
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2021-07-03 17:39:32 +0700

    dulang mahasiswa aktif

--- frontend/src/views/pages/admin/akademik/DulangMahasiswaAktif.vue
+++ frontend/src/views/pages/admin/akademik/DulangMahasiswaAktif.vue
@@ -5,10 +5,12 @@
 				mdi-account-box-multiple
 			</template>
 			<template v-slot:name>
-				DAFTAR ULANG MAHASISWA AKTIF 
+				DAFTAR ULANG MAHASISWA AKTIF
 			</template>
 			<template v-slot:subtitle>
-				TAHUN AKADEMIK {{ tahun_akademik }} SEMESTER {{ $store.getters["uiadmin/getNamaSemester"](semester_akademik) }} - {{ nama_prodi }}
+				TAHUN AKADEMIK {{ tahun_akademik }} SEMESTER
+				{{ $store.getters["uiadmin/getNamaSemester"](semester_akademik) }} -
+				{{ nama_prodi }}
 			</template>
 			<template v-slot:breadcrumbs>
 				<v-breadcrumbs :items="breadcrumbs" class="pa-0">
@@ -18,20 +20,20 @@
 				</v-breadcrumbs>
 			</template>
 			<template v-slot:desc>
-				<v-alert                                        
-					color="cyan"
-					border="left"  
-					colored-border
-					type="info"
-					>
+				<v-alert color="cyan" border="left" colored-border type="info">
 					Halaman untuk melihat daftar mahasiswa yang aktif.
 				</v-alert>
 			</template>
 		</ModuleHeader>
 		<template v-slot:filtersidebar>
-			<Filter6 v-on:changeTahunAkademik="changeTahunAkademik" v-on:changeSemesterAkademik="changeSemesterAkademik" v-on:changeProdi="changeProdi" ref="filter6" />	
+			<Filter6
+				v-on:changeTahunAkademik="changeTahunAkademik"
+				v-on:changeSemesterAkademik="changeSemesterAkademik"
+				v-on:changeProdi="changeProdi"
+				ref="filter6"
+			/>
 		</template>
-		<v-container fluid> 
+		<v-container fluid>
 			<v-row class="mb-4" no-gutters>
 				<v-col cols="12">
 					<v-card>
@@ -53,7 +55,7 @@
 						:headers="headers"
 						:items="datatable"
 						:search="search"
-						item-key="id"   
+						item-key="id"
 						show-expand
 						:expanded.sync="expanded"
 						:single-expand="true"
@@ -62,38 +64,47 @@
 						@click:row="dataTableRowClicked"
 						class="elevation-1"
 						:loading="datatableLoading"
-						loading-text="Loading... Please wait">
+						loading-text="Loading... Please wait"
+					>
 						<template v-slot:top>
 							<v-toolbar flat color="white">
 								<v-toolbar-title>DAFTAR MAHASISWA</v-toolbar-title>
-								<v-divider
-									class="mx-4"
-									inset
-									vertical
-								></v-divider>
+								<v-divider class="mx-4" inset vertical />
 								<v-spacer></v-spacer>
 							</v-toolbar>
 						</template>
 						<template v-slot:item.idkelas="{ item }">
-							{{$store.getters['uiadmin/getNamaKelas'](item.idkelas)}}
+							{{ $store.getters["uiadmin/getNamaKelas"](item.idkelas) }}
 						</template>
-						<template v-slot:item.actions v-if="dashboard== 'mahasiswa'">
+						<template v-slot:item.actions v-if="dashboard == 'mahasiswa'">
 							N.A
 						</template>
 						<template v-slot:item.actions="{ item }" v-else>
-							<v-icon
-								small
-								:disabled="btnLoading"
-								@click.stop="deleteItem(item)">
-								mdi-delete
-							</v-icon>
+							<v-tooltip bottom>
+								<template v-slot:activator="{ on, attrs }">
+									<v-icon
+										v-bind="attrs"
+										v-on="on"
+										small
+										color="red darken-1"
+										:disabled="btnLoading"
+										@click.stop="deleteItem(item)"
+									>
+										mdi-delete
+									</v-icon>
+								</template>
+								<span>Hapus Daftar Ulang Aktif</span>
+							</v-tooltip>
 						</template>
 						<template v-slot:expanded-item="{ headers, item }">
 							<td :colspan="headers.length" class="text-center">
-								<v-col cols="12">  
-									<strong>id:</strong>{{ item.id }} 
-									<strong>created_at:</strong>{{ $date(item.created_at).format("DD/MM/YYYY HH:mm") }}
-									<strong>updated_at:</strong>{{ $date(item.updated_at).format("DD/MM/YYYY HH:mm") }}
+								<v-col cols="12">
+									<strong>id:</strong>
+									{{ item.id }}
+									<strong>created_at:</strong>
+									{{ $date(item.created_at).format("DD/MM/YYYY HH:mm") }}
+									<strong>updated_at:</strong>
+									{{ $date(item.updated_at).format("DD/MM/YYYY HH:mm") }}
 								</v-col>
 							</td>
 						</template>
@@ -111,39 +122,45 @@
 	import ModuleHeader from "@/components/ModuleHeader";
 	import Filter6 from "@/components/sidebar/FilterMode6";
 	export default {
-		name: 'DulangMahasiswaBaru',
+		name: "DulangMahasiswaBaru",
 		created() {
-			this.dashboard = this.$store.getters["uiadmin/getDefaultDashboard"]; 
+			this.dashboard = this.$store.getters["uiadmin/getDefaultDashboard"];
 			this.breadcrumbs = [
 				{
 					text: "HOME",
 					disabled: false,
-					href: "/dashboard/" + this.$store.getters["auth/AccessToken"]
+					href: "/dashboard/" + this.$store.getters["auth/AccessToken"],
 				},
 				{
 					text: "AKADEMIK",
 					disabled: false,
-					href: "/akademik"
+					href: "/akademik",
 				},
 				{
-					text: 'DAFTAR ULANG',
+					text: "DAFTAR ULANG",
 					disabled: false,
-					href: "#"
+					href: "#",
 				},
 				{
-					text: 'DAFTAR ULANG MAHASISWA LAMA',
+					text: "DAFTAR ULANG MAHASISWA LAMA",
 					disabled: true,
-					href: "#"
-				}
+					href: "#",
+				},
 			];
 			let prodi_id = this.$store.getters["uiadmin/getProdiID"];
 			this.prodi_id = prodi_id;
 			this.nama_prodi = this.$store.getters["uiadmin/getProdiName"](prodi_id);
 			this.tahun_akademik = this.$store.getters["uiadmin/getTahunAkademik"];
-			this.semester_akademik = this.$store.getters["uiadmin/getSemesterAkademik"]; 
-			this.initialize()
+			this.semester_akademik = this.$store.getters[
+				"uiadmin/getSemesterAkademik"
+			];
+			this.initialize();
 		},
-		data: () => ({ 
+		mounted() {
+			this.firstloading = false;
+			this.$refs.filter6.setFirstTimeLoading(this.firstloading);
+		},
+		data: () => ({
 			dashboard: null,
 			firstloading: true,
 			prodi_id: null,
@@ -155,90 +172,108 @@
 			btnLoadingTable: false,
 			datatableLoading: false,
 			expanded: [],
-			datatable: [], 
+			datatable: [],
 			headers: [
-				{ text: 'NO. FORMULIR', value: 'no_formulir', sortable: true, width: 100 },
+				{
+					text: "NO. FORMULIR",
+					value: "no_formulir",
+					sortable: true,
+					width: 100,
+				},
 				{ text: "NIM", value: "nim", sortable: true, width: 100 },
-				{ text: 'NIRM', value: 'nirm', sortable: true, width: 100 },
-				{ text: "NAMA MAHASISWA", value: "nama_mhs", sortable: true, width: 250 }, 
-				{ text: 'KELAS', value: 'idkelas', sortable: true, width: 120, }, 
-				{ text: 'DOSEN WALI', value: 'dosen_wali', sortable: true, width: 200, }, 
+				{ text: "NIRM", value: "nirm", sortable: true, width: 100 },
+				{
+					text: "NAMA MAHASISWA",
+					value: "nama_mhs",
+					sortable: true,
+					width: 250,
+				},
+				{ text: "KELAS", value: "idkelas", sortable: true, width: 120 },
+				{ text: "DOSEN WALI", value: "dosen_wali", sortable: true, width: 200 },
 				{ text: "AKSI", value: "actions", sortable: false, width: 70 },
 			],
-			search: "", 
+			search: "",
 		}),
 		methods: {
-			changeTahunAkademik(tahun)
-			{
+			changeTahunAkademik(tahun) {
 				this.tahun_akademik = tahun;
 			},
-			changeSemesterAkademik(semester)
-			{
-				this.semester_akademik = semester; 
+			changeSemesterAkademik(semester) {
+				this.semester_akademik = semester;
 			},
-			changeProdi(id)
-			{
+			changeProdi(id) {
 				this.prodi_id = id;
 			},
-			initialize: async function() 
-			{
+			initialize: async function() {
 				this.datatableLoading = true;
-				await this.$ajax.post('/akademik/dulang/mhsaktif',
-				{
-					prodi_id: this.prodi_id,
-					ta: this.tahun_akademik,
-					idsmt: this.semester_akademik,
-				},
-				{
-					headers: {
-						Authorization: this.$store.getters["auth/Token"]
-					}
-				}).then(({ data }) => {
-					this.datatable = data.mahasiswa;
-					this.datatableLoading = false;
-				}).catch(() => {
-					this.datatableLoading = false;
-				});
-				this.firstloading = false;
-				this.$refs.filter6.setFirstTimeLoading(this.firstloading); 
+				await this.$ajax
+					.post(
+						"/akademik/dulang/mhsaktif",
+						{
+							prodi_id: this.prodi_id,
+							ta: this.tahun_akademik,
+							idsmt: this.semester_akademik,
+						},
+						{
+							headers: {
+								Authorization: this.$store.getters["auth/Token"],
+							},
+						}
+					)
+					.then(({ data }) => {
+						this.datatable = data.mahasiswa;
+						this.datatableLoading = false;
+					})
+					.catch(() => {
+						this.datatableLoading = false;
+					});
 			},
-			dataTableRowClicked(item)
-			{
-				if (item === this.expanded[0])
-				{
+			dataTableRowClicked(item) {
+				if (item === this.expanded[0]) {
 					this.expanded = [];
-				}
-				else
-				{
+				} else {
 					this.expanded = [item];
 				}
 			},
-			deleteItem(item)
-			{
-				this.$root.$confirm.open("Delete", 'Apakah Anda ingin menghapus daftar ulang '+item.nama_mhs+' ?', { color: 'red', width:600,'desc': 'proses ini juga menghapus seluruh data akademik namun KEUANGAN TETAP ADA.' }).then(confirm => {
-					if (confirm)
-					{
-						this.btnLoadingTable=true;
-						this.$ajax.post('/akademik/dulang/mhsaktif'+item.id,
-							{
-								_method: "DELETE",
-							},
-							{
-								headers: {
-									Authorization: this.$store.getters["auth/Token"]
-								}
-							}
-						).then(() => {
-							const index = this.datatable.indexOf(item);
-							this.datatable.splice(index, 1);
-							this.btnLoadingTable=false;
-						}).catch(() => {
-							this.btnLoadingTable=false;
-						});
-					} 
-				});
+			deleteItem(item) {
+				this.$root.$confirm
+					.open(
+						"Delete",
+						"Apakah Anda ingin menghapus daftar ulang " + item.nama_mhs + " ?",
+						{
+							color: "red",
+							width: 600,
+							desc:
+								"proses ini juga menghapus seluruh data akademik namun KEUANGAN TETAP ADA.",
+						}
+					)
+					.then(confirm => {
+						if (confirm) {
+							this.btnLoadingTable = true;
+							this.$ajax
+								.post(
+									"/akademik/dulang/mhsaktif" + item.id,
+									{
+										_method: "DELETE",
+									},
+									{
+										headers: {
+											Authorization: this.$store.getters["auth/Token"],
+										},
+									}
+								)
+								.then(() => {
+									const index = this.datatable.indexOf(item);
+									this.datatable.splice(index, 1);
+									this.btnLoadingTable = false;
+								})
+								.catch(() => {
+									this.btnLoadingTable = false;
+								});
+						}
+					});
 			},
-			closedialogfrm() { 
+			closedialogfrm() {
 				this.dialogfrm = false;
 				setTimeout(() => {
 					this.formdata = Object.assign({}, this.formdefault);
