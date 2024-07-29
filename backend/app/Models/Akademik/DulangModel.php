69791fa84dcc2697c182203aaab1d6395b3c37c3
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2024-07-29 21:48:56 +0700

    validasi email dan no hp

--- backend/app/Models/Akademik/DulangModel.php
+++ backend/app/Models/Akademik/DulangModel.php
@@ -6,57 +6,56 @@ use Illuminate\Database\Eloquent\Model;
 
 class DulangModel extends Model 
 {    
-	 /**
-	 * nama tabel model ini.
-	 *
-	 * @var string
-	 */
-	protected $table = 'pe3_dulang';
-	/**
-	 * primary key tabel ini.
-	 *
-	 * @var string
-	 */
-	protected $primaryKey = 'id';
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $fillable = [
-		'id',        
-		'user_id',
-		'nim',
-		'tahun', 
-		'idsmt', 
-		'tasmt', 
-		'idkelas', 
-		'status_sebelumnya',
-		'k_status',
-		'update_info',
-		'descr',
-	];
-	/**
-	 * enable auto_increment.
-	 *
-	 * @var string
-	 */
-	public $incrementing = false;
-	/**
-	 * activated timestamps.
-	 *
-	 * @var string
-	 */
-	public $timestamps = true;
-	
-	public function register_mahasiswa()
-	{
-		return $this->belongsTo('App\Models\Akademik\RegisterMahasiswaModel', 'user_id', 'user_id');
-	}
-
-	public function user()
-	{
-		return $this->belongsTo('App\Models\User','user_id','id');
-	}
+   /**
+   * nama tabel model ini.
+   *
+   * @var string
+   */
+  protected $table = 'pe3_dulang';
+  /**
+   * primary key tabel ini.
+   *
+   * @var string
+   */
+  protected $primaryKey = 'id';
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'id',        
+    'user_id',
+    'nim',
+    'tahun', 
+    'idsmt', 
+    'tasmt', 
+    'idkelas', 
+    'status_sebelumnya',
+    'k_status',
+    'update_info',
+    'descr',
+  ];
+  /**
+   * enable auto_increment.
+   *
+   * @var string
+   */
+  public $incrementing = false;
+  /**
+   * activated timestamps.
+   *
+   * @var string
+   */
+  public $timestamps = true;
+  
+  public function register_mahasiswa()
+  {
+    return $this->belongsTo('App\Models\Akademik\RegisterMahasiswaModel', 'user_id', 'user_id');
+  }
+  public function user()
+  {
+    return $this->belongsTo('App\Models\User','user_id','id');
+  }
 
 }
