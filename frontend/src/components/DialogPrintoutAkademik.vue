ba101c5556fbf6c65ee808aa2186a27404ed5040
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2025-12-31 09:38:26 +0700

    ganti layout

--- frontend/src/components/DialogPrintoutAkademik.vue
+++ frontend/src/components/DialogPrintoutAkademik.vue
@@ -1,56 +1,94 @@
 <template>
-	<v-dialog v-model="dialog" max-width="500px" @keydown.esc="cancel">
-		<v-card>
-			<v-toolbar dark color="primary" dense flat>
-				<v-toolbar-title class="white--text">
-					Printout {{ title }}  
-				</v-toolbar-title>
-				<v-spacer></v-spacer> 
-				<v-icon>mdi-printer</v-icon> 
-			</v-toolbar>
-			<v-card-text>
-				{{ message }}<br />	
-				<v-btn
-					color="green"
-					text
-					:href="$api.storageURL + '/' + file"> 
-					Download
-			</v-btn> 
-			</v-card-text>
-		</v-card>
-	</v-dialog>
+  <v-dialog v-model="dialog" max-width="500px" persistent @keydown.esc="cancel">
+    <v-card class="rounded-lg" elevation="8">
+      <v-toolbar dark color="primary" dense flat class="elevation-0">
+        <v-icon left class="mr-2">mdi-printer</v-icon>
+        <v-toolbar-title class="text-h6 font-weight-medium">
+          Printout {{ title }}
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon dark @click="cancel">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-toolbar>
+
+      <v-card-text class="pa-6">
+        <div class="d-flex align-center mb-4">
+          <v-icon color="success" size="40" class="mr-3">mdi-file-check</v-icon>
+          <div class="flex-grow-1">
+            <p class="body-1 mb-0 text--primary font-weight-medium">
+              {{ message }}
+            </p>
+            <p v-if="nama_file" class="caption text--secondary mt-1 mb-0">
+              {{ nama_file }}
+            </p>
+          </div>
+        </div>
+
+        <v-divider class="my-4"></v-divider>
+
+        <div class="text-center">
+          <v-btn
+            color="success"
+            large
+            depressed
+            :href="$api.storageURL + '/' + file"
+            target="_blank"
+            class="px-6 text-capitalize"
+            elevation="2"
+          >
+            <v-icon left>mdi-download</v-icon>
+            Download File
+          </v-btn>
+        </div>
+      </v-card-text>
+
+      <v-card-actions class="px-6 pb-4">
+        <v-spacer></v-spacer>
+        <v-btn
+          text
+          @click="cancel"
+          class="text-capitalize"
+        >
+          Tutup
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 <script>
-	export default {
-		name: "DialogPrintoutAkademik",
-		props: {
-			pid: {
-				type: String,
-				required: true,
-			}, 
-			title: {
-				type: String,
-				default: "",
-				required: true,
-			},
-		},
-		data: () => ({
-			dialog: false,
-			message: "",
-			file: "",
-			nama_file: "",
-		}),
-		methods: {
-			open(item) {
-				this.message = item.message;
-				this.file = item.file;
-				this.nama_file = item.nama_file;
-				this.dialog = true;
-			},
-			cancel() {
-        this.resolve(false);
+  export default {
+    name: "DialogPrintoutAkademik",
+    props: {
+      pid: {
+        type: String,
+        required: true,
+      }, 
+      title: {
+        type: String,
+        default: "",
+        required: true,
+      },
+    },
+    data: () => ({
+      dialog: false,
+      message: "",
+      file: "",
+      nama_file: "",
+    }),
+    methods: {
+      open(item = {}) {
+        this.message = item.message || "";
+        this.file = item.file || "";
+        this.nama_file = item.nama_file || "";
+        this.dialog = true;
+      },
+      cancel() {
+        if (this.resolve) {
+          this.resolve(false);
+        }
         this.dialog = false;
-			}
-		},
-	};
+      }
+    },
+  };
 </script>
--- frontend/src/components/ProfilMahasiswaLama.vue
+++ frontend/src/components/ProfilMahasiswaLama.vue
@@ -1,155 +1,262 @@
 <template>
-    <v-card color="grey lighten-4">
-        <v-toolbar elevation="2"> 
-            <v-toolbar-title>PROFIL MAHASISWA</v-toolbar-title>
-            <v-divider
-                class="mx-4"
-                inset
-                vertical
-            ></v-divider>
-            <v-spacer></v-spacer>
-            <v-icon                
-                @click.stop="exit()">
-                mdi-close-thick
-            </v-icon>
-        </v-toolbar>
-        <v-card-text v-if="datamhs.hasOwnProperty('user_id')">
-            <v-row>
-                <v-col xs="12" sm="12" md="2">
-                    <v-card flat class="mb-2">
-                        <v-img :src="$api.url + '/' + datamhs.foto" />
-                        <v-card-title>NOMOR HP:</v-card-title>  
-                        <v-card-subtitle>
-                            {{datamhs.nomor_hp}}
-                        </v-card-subtitle>
-                        <v-card-title>USERNAME:</v-card-title>  
-                        <v-card-subtitle>
-                            {{datamhs.nim}}
-                        </v-card-subtitle>
-                        <v-divider class="mx-4"></v-divider>
-                        <v-card-text>
-                            <v-chip label outlined color="info">{{datamhs.n_status}}</v-chip>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-                <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly || $vuetify.breakpoint.smOnly"/>
-                <v-col xs="12" sm="12" md="10">
-                    <v-row>
-                        <v-col xs="12" sm="12" md="6">
-                            <v-card flat class="mb-2">
-                                <v-card-title>USER ID:</v-card-title>  
-                                <v-card-subtitle>
-                                    {{datamhs.user_id}}
-                                </v-card-subtitle>
-                            </v-card>
-                        </v-col>
-                        <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly || $vuetify.breakpoint.smOnly"/>
-                        <v-col xs="12" sm="12" md="6">
-                            <v-card flat class="mb-2">
-                                <v-card-title>PROGRAM STUDI / KELAS:</v-card-title>  
-                                <v-card-subtitle>
-                                    {{datamhs.nama_prodi}} / {{datamhs.nkelas}}
-                                </v-card-subtitle>
-                            </v-card>
-                        </v-col>
-                        <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly || $vuetify.breakpoint.smOnly"/>
-                    </v-row>
-                    <v-row>
-                        <v-col xs="12" sm="12" md="6">
-                            <v-card flat class="mb-2">
-                                <v-card-title>NIM / NIRM / NO. FORMULIR:</v-card-title>  
-                                <v-card-subtitle>
-                                    {{datamhs.nim}} / {{datamhs.nirm}} / {{datamhs.no_formulir}}
-                                </v-card-subtitle>
-                            </v-card>
-                        </v-col>
-                        <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly || $vuetify.breakpoint.smOnly"/>
-                        <v-col xs="12" sm="12" md="6">
-                            <v-card flat class="mb-2">
-                                <v-card-title>TAHUN PENDAFTARAN:</v-card-title>  
-                                <v-card-subtitle>
-                                    {{datamhs.tahun}}
-                                </v-card-subtitle>
-                            </v-card>
-                        </v-col>
-                        <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly || $vuetify.breakpoint.smOnly"/>
-                    </v-row>
-                    <v-row>
-                        <v-col xs="12" sm="12" md="6">
-                            <v-card flat class="mb-2">
-                                <v-card-title>NAMA MAHASISWA:</v-card-title>  
-                                <v-card-subtitle>
-                                    {{datamhs.nama_mhs}} [{{datamhs.jk}}]
-                                </v-card-subtitle>
-                            </v-card>
-                        </v-col>
-                        <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly || $vuetify.breakpoint.smOnly"/>
-                        <v-col xs="12" sm="12" md="6">
-                            <v-card flat class="mb-2">
-                                <v-card-title>ALAMAT:</v-card-title>  
-                                <v-card-subtitle>
-                                    {{datamhs.alamat_rumah}}
-                                </v-card-subtitle>
-                            </v-card>
-                        </v-col>
-                        <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly || $vuetify.breakpoint.smOnly"/>
-                    </v-row>
-                    <v-row>
-                        <v-col xs="12" sm="12" md="6">
-                            <v-card flat class="mb-2">
-                                <v-card-title>EMAIL:</v-card-title>  
-                                <v-card-subtitle>
-                                    {{datamhs.email}}
-                                </v-card-subtitle>
-                            </v-card>
-                        </v-col>
-                        <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly || $vuetify.breakpoint.smOnly"/>
-                        <v-col xs="12" sm="12" md="6">
-                            <v-card flat class="mb-2">
-                                <v-card-title>CREATED/UPDATED:</v-card-title>  
-                                <v-card-subtitle>
-                                    {{ $date(datamhs.created_at).format("DD/MM/YYYY HH:mm") }} ~ {{ $date(datamhs.updated_at).format("DD/MM/YYYY HH:mm") }}
-                                </v-card-subtitle>
-                            </v-card>
-                        </v-col>
-                        <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly || $vuetify.breakpoint.smOnly"/>
-                    </v-row>
-                </v-col>
-                <v-responsive width="100%" v-if="$vuetify.breakpoint.xsOnly || $vuetify.breakpoint.smOnly"/>
-            </v-row>
-        </v-card-text>
-    </v-card>
+  <v-card class="profile-card" elevation="2">
+    <v-toolbar color="primary" dark elevation="0" class="profile-toolbar">
+      <v-icon left class="ml-2">mdi-account-circle</v-icon>
+      <v-toolbar-title class="font-weight-bold">PROFIL MAHASISWA</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        icon
+        dark
+        @click.stop="exit()"
+        class="mr-2"
+      >
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </v-toolbar>
+    
+    <v-card-text v-if="datamhs.hasOwnProperty('user_id')" class="pa-4">
+      <v-row>
+        <!-- Photo & Quick Info Section -->
+        <v-col cols="12" md="3" class="d-flex flex-column">
+          <v-card 
+            elevation="3" 
+            class="photo-card mb-4 text-center"
+            rounded="lg"
+          >
+            <v-img 
+              :src="$api.url + '/' + datamhs.foto" 
+              class="profile-photo"
+              aspect-ratio="1"
+              cover
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-icon size="64" color="grey lighten-1">mdi-account</v-icon>
+                </v-row>
+              </template>
+            </v-img>
+            <v-card-text class="pa-4">
+              <v-chip 
+                :color="getStatusColor(datamhs.n_status)" 
+                dark 
+                class="mb-3"
+                large
+                label
+              >
+                <v-icon left small>mdi-information</v-icon>
+                {{ datamhs.n_status }}
+              </v-chip>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <!-- Main Information Section -->
+        <v-col cols="12" md="9">
+          <v-row dense>
+            <!-- Nama Mahasiswa - Highlighted -->
+            <v-col cols="12">
+              <v-card 
+                elevation="2" 
+                class="info-card highlight-card"
+                rounded="lg"
+              >
+                <v-card-text class="pa-4">
+                  <div class="d-flex align-center mb-2">
+                    <v-icon color="primary" class="mr-2">mdi-account</v-icon>
+                    <span class="text-caption grey--text text--darken-1 font-weight-medium">NAMA MAHASISWA</span>
+                  </div>
+                  <div class="text-h6 primary--text font-weight-bold">
+                    {{ datamhs.nama_mhs }}
+                    <v-chip x-small class="ml-2" color="secondary" text-color="white">
+                      {{ datamhs.jk }}
+                    </v-chip>
+                  </div>
+                  <v-divider class="my-3"></v-divider>
+                  <v-row dense>
+                    <v-col cols="12" sm="4">
+                      <div class="text-caption grey--text text--darken-1 mb-1">Nomor HP</div>
+                      <div class="text-body-2 font-weight-medium">{{ datamhs.nomor_hp || '-' }}</div>
+                    </v-col>
+                    <v-col cols="12" sm="4">
+                      <div class="text-caption grey--text text--darken-1 mb-1">Username</div>
+                      <div class="text-body-2 font-weight-medium">{{ datamhs.nim || '-' }}</div>
+                    </v-col>
+                    <v-col cols="12" sm="4">
+                      <div class="text-caption grey--text text--darken-1 mb-1">CREATED / UPDATED</div>
+                      {{ $date(datamhs.created_at).format("DD/MM/YYYY HH:mm") }} 
+                      <v-icon size="14" class="mx-2">mdi-arrow-right</v-icon>
+                      {{ $date(datamhs.updated_at).format("DD/MM/YYYY HH:mm") }}
+                    </v-col>
+                  </v-row>                                    
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <!-- User ID -->
+            <v-col cols="12" sm="6" md="4">
+              <v-card elevation="2" class="info-card" rounded="lg">
+                <v-card-text class="pa-3">
+                  <div class="d-flex align-center mb-1">
+                    <v-icon color="primary" size="18" class="mr-2">mdi-identifier</v-icon>
+                    <span class="text-caption grey--text text--darken-1">USER ID</span>
+                  </div>
+                  <div class="text-body-1 font-weight-medium">{{ datamhs.user_id }}</div>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <!-- Program Studi / Kelas -->
+            <v-col cols="12" sm="6" md="4">
+              <v-card elevation="2" class="info-card" rounded="lg">
+                <v-card-text class="pa-3">
+                  <div class="d-flex align-center mb-1">
+                    <v-icon color="primary" size="18" class="mr-2">mdi-school</v-icon>
+                    <span class="text-caption grey--text text--darken-1">PROGRAM STUDI / KELAS</span>
+                  </div>
+                  <div class="text-body-1 font-weight-medium">
+                    {{ datamhs.nama_prodi }} / {{ datamhs.nkelas }}
+                  </div>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <!-- Tahun Pendaftaran -->
+            <v-col cols="12" sm="6" md="4">
+              <v-card elevation="2" class="info-card" rounded="lg">
+                <v-card-text class="pa-3">
+                  <div class="d-flex align-center mb-1">
+                    <v-icon color="primary" size="18" class="mr-2">mdi-calendar</v-icon>
+                    <span class="text-caption grey--text text--darken-1">TAHUN PENDAFTARAN</span>
+                  </div>
+                  <div class="text-body-1 font-weight-medium">{{ datamhs.tahun }}</div>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <!-- NIM / NIRM / No. Formulir -->
+            <v-col cols="12" sm="6" md="4">
+              <v-card elevation="2" class="info-card" rounded="lg">
+                <v-card-text class="pa-3">
+                  <div class="d-flex align-center mb-1">
+                    <v-icon color="primary" size="18" class="mr-2">mdi-card-account-details</v-icon>
+                    <span class="text-caption grey--text text--darken-1">NIM / NIRM / NO. FORMULIR</span>
+                  </div>
+                  <div class="text-body-2 font-weight-medium">
+                    {{ datamhs.nim }} / {{ datamhs.nirm }} / {{ datamhs.no_formulir }}
+                  </div>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <!-- Email -->
+            <v-col cols="12" sm="6" md="4">
+              <v-card elevation="2" class="info-card" rounded="lg">
+                <v-card-text class="pa-3">
+                  <div class="d-flex align-center mb-1">
+                    <v-icon color="primary" size="18" class="mr-2">mdi-email</v-icon>
+                    <span class="text-caption grey--text text--darken-1">EMAIL</span>
+                  </div>
+                  <div class="text-body-2 font-weight-medium">{{ datamhs.email || '-' }}</div>
+                </v-card-text>
+              </v-card>
+            </v-col>
+
+            <!-- Alamat -->
+            <v-col cols="12" sm="6" md="4">
+              <v-card elevation="2" class="info-card" rounded="lg">
+                <v-card-text class="pa-3">
+                  <div class="d-flex align-center mb-1">
+                    <v-icon color="primary" size="18" class="mr-2">mdi-map-marker</v-icon>
+                    <span class="text-caption grey--text text--darken-1">ALAMAT</span>
+                  </div>
+                  <div class="text-body-2 font-weight-medium">{{ datamhs.alamat_rumah || '-' }}</div>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
 <script>
-export default {
+  export default {
     name: 'ProfilMahasiswaLama',
-    created()
-    {
-        this.initialize(); 
-   },
+    created() {
+      this.initialize(); 
+    },
     props: {
-        datamhs: {
-            type: Object,
-            required: true
-       },
-        url: {
-            type: String,
-            default: null            
-        }
-   },
+      datamhs: {
+        type: Object,
+        required: true
+      },
+      url: {
+        type: String,
+        default: null            
+      }
+    },
     
     methods: {
-        initialize: async function()
-        {
-            
-       },
-        exit()
-        {
-            if (this.url != null)
-            {
-                this.$router.push(this.url);
-            } 
-        }
-   }, 
-}
+      initialize: async function() {
+        // Initialize if needed
+      },
+      exit() {
+        if (this.url != null) {
+          this.$router.push(this.url);
+        } 
+      },
+      getStatusColor(status) {
+        const statusColors = {
+          'Aktif': 'success',
+          'Lulus': 'info',
+          'Cuti': 'warning',
+          'Nonaktif': 'error',
+          'Drop Out': 'error'
+        };
+        return statusColors[status] || 'primary';
+      }
+    }
+  }
 </script>
+
+<style scoped>
+  .profile-card {
+    border-radius: 12px;
+    overflow: hidden;
+  }
+
+  .profile-toolbar {
+    background: linear-gradient(135deg, #667eea 0%, #764ba2 100%) !important;
+  }
+
+  .photo-card {
+    position: sticky;
+    top: 20px;
+  }
+
+  .profile-photo {
+    border-radius: 8px 8px 0 0;
+  }
+
+  .info-card {
+    transition: all 0.3s ease;
+    height: 100%;
+  }
+
+  .info-card:hover {
+    transform: translateY(-2px);
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15) !important;
+  }
+
+  .highlight-card {
+    background: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
+    border-left: 4px solid #667eea;
+  }
+
+  @media (max-width: 960px) {
+    .photo-card {
+      position: relative;
+      top: 0;
+    }
+  }
+</style>
